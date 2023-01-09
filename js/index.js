53c22c33fb5cdd3e42a5d931614e27d3c9169a07
Maksim <cernovm322@gmail.com>
2023-01-09 17:24:48 +0300

    1

--- js/index.js
+++ js/index.js
@@ -11,3 +11,11 @@ window.addEventListener("DOMContentLoaded", function () {
       document.querySelector("#pop_up").classList.toggle("active");
     });
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#close-pop_up_x")
+    .addEventListener("click", function () {
+      document.querySelector("#pop_up").classList.toggle("active");
+    });
+});
